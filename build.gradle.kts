24250f0a4de38143244f92e81f51430f67a8ab5b
lordpipe <lordpipe@protonmail.com>
2023-10-05 02:28:28 -0600

    update upstream, remove unnecessary patches

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,8 @@ plugins {
 
     id("com.github.johnrengelman.shadow") version "8.1.1" apply false
     // keep in sync with upstream (https://github.com/PaperMC/Paper/blob/master/build.gradle.kts)
-    id("io.papermc.paperweight.patcher") version "1.5.5"
+    // id("io.papermc.paperweight.patcher") version "1.5.6"
+    id("io.papermc.paperweight.patcher")
 }
 
 val paperMavenPublicUrl = "https://repo.papermc.io/repository/maven-public/"
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,14 @@ pluginManagement {
     repositories {
         gradlePluginPortal()
         maven("https://repo.papermc.io/repository/maven-public/")
+        maven("https://jitpack.io")
+    }
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.id == "io.papermc.paperweight.patcher") {
+                useModule("com.github.PaperMC.paperweight:paperweight-patcher:4803719449") // can also be a branch, i.e. `master-SNAPSHOT`
+            }
+        }
     }
 }
 
