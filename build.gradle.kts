132c35a2a1110d4055375df3f81c77fe6bff8383
lordpipe <lordpipe@protonmail.com>
2023-12-06 03:56:06 -0700

    update upstream

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
     id("com.github.johnrengelman.shadow") version "8.1.1" apply false
     // keep in sync with upstream (https://github.com/PaperMC/Paper/blob/master/build.gradle.kts)
-    id("io.papermc.paperweight.patcher") version "1.5.8"
+    id("io.papermc.paperweight.patcher") version "1.5.10"
 }
 
 val paperMavenPublicUrl = "https://repo.papermc.io/repository/maven-public/"
@@ -18,7 +18,7 @@ repositories {
 
 dependencies {
     // keep in sync with upstream (https://github.com/PaperMC/Paper/blob/master/build.gradle.kts)
-    remapper("net.fabricmc:tiny-remapper:0.8.6:fat")
+    remapper("net.fabricmc:tiny-remapper:0.8.10:fat")
     // keep in sync with upstream (https://github.com/PaperMC/Paper/blob/master/build.gradle.kts)
     decompiler("net.minecraftforge:forgeflower:2.0.627.2")
     // keep in sync with upstream (https://github.com/PaperMC/Paper/blob/master/build.gradle.kts)
