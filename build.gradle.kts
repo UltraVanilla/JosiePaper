36f47bb3592ea8687ed61177d6b7c7340ea0a713
lordpipe <lordpipe@protonmail.com>
2023-12-12 08:26:04 -0700

    update upstream

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
     id("com.github.johnrengelman.shadow") version "8.1.1" apply false
     // keep in sync with upstream (https://github.com/PaperMC/Paper/blob/master/build.gradle.kts)
-    id("io.papermc.paperweight.patcher") version "1.5.10"
+    id("io.papermc.paperweight.patcher") version "1.5.11"
 }
 
 val paperMavenPublicUrl = "https://repo.papermc.io/repository/maven-public/"
